1bf99e190e2dde8e28fdb8e8844ccad4a7033c38
Megan <megisalia@gmail.com>
2022-10-14 23:58:50 +0400

    commit

---
+++ Fraction.java
@@ -0,0 +1,8 @@
+public class Fraction {
+    int numerator;
+    int denominator;
+}
+
+
+
+
---
+++ Main.java
@@ -0,0 +1,75 @@
+//// - #1 exercise
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        int a = 10;
+//        int b = 40;
+//        int c = 30;
+//
+//        int desc = ((b * b) - (4 * a * c));
+//
+//        System.out.println("Discriminant: " + desc);
+//    }
+//}
+
+
+//// - #2 exercise
+//public class Main {
+//
+//        public static void main(String[] args) {
+//
+//        String[] words = {"Java", "Kotlin", "Flutter", "React Native", "Swift", "Dart"};
+//        String smallest = words[0];
+//
+//        for (String word : words) {
+//                if (smallest.length() > word.length()) {
+//                        smallest = word;
+//                }
+//        }
+//
+//        System.out.println(smallest);
+//        }
+//}
+
+// - #3 exercise
+//public class Main {
+//
+//        public static void main(String[] args) {
+//
+//        Fraction f1 = new Fraction();
+//        f1.numerator = 18;
+//        f1.denominator = 72;
+//
+//        Fraction f2 = new Fraction();
+//        f2.numerator = 12;
+//        f2.denominator = 24;
+//
+//
+//        // Fraction truncation
+//        int newNum = f1.numerator / 3;
+//        int newDenom = f1.denominator / 3;
+//
+//        System.out.println("Fraction truncation: " + newNum + " / " + newDenom);
+//
+//
+//        // Fraction multiplication
+//        int newNum2 = f2.numerator;
+//        int newDenom2 = f2.denominator;
+//
+//        int multiplyNumerator = newNum * newNum2;
+//        int multiplyDenominator = newDenom * newDenom2;
+//
+//        System.out.println("Fraction multiplication: " + multiplyNumerator + " / " + multiplyDenominator);
+//
+//
+//        // Fraction addition
+//        int plusNumerator = newNum * newDenom2 + newNum2 * newDenom;
+//        int plusDenominator = newDenom * newDenom2;
+//
+//        System.out.println("Fraction addition: " + plusNumerator + " / " + plusDenominator);
+//
+//    }
+//}
+
+
